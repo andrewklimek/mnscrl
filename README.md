1e0aa42b0caf5d5e006733e6d6c040faa8fc3d5f
Andrew Klimek <andrew.klimek@gmail.com>
2017-06-26 18:55:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mnscrl
+
+Mnml + Scroll = mnmscrl
+
+A minimalist animated scrollTo function
+
+Only 293 characters minified
+
+Uses requestAnimationFrame for performance
+
+Uses an exponential ease-out function
+
+Use it if you don’t need jQuery for anything else
---
+++ mnscrl.js
@@ -0,0 +1,26 @@
+// position: the target scrollY property of the window
+// speed: time in pixels per second
+function easeTo(position, speed) {
+
+	var scrollY = window.scrollY || document.documentElement.scrollTop,// documentElement.scrollTop is for IE
+	speed = speed || 1500,// 1500 pps default
+	currentTime = 0,
+	time = Math.max(.1, Math.min(Math.abs(scrollY - position) / speed, .8));// min time .1, max time .8 seconds
+
+	function frame() {
+		currentTime += 1 / 60;		
+
+		var p = currentTime / time;
+		
+		if (p < 1) {
+			window.requestAnimationFrame(frame);// || window.setTimeout(frame, 100/6);// if you need IE9
+			var t = -Math.pow(2, -10 * p) + 1;// easeOutExpo from https://github.com/danro/easing-js
+			window.scrollTo(0, scrollY + ((position - scrollY) * t));
+		} else {
+			// console.log('scroll done');
+			window.scrollTo(0, position);
+		}
+	}
+
+	frame();// call it once to get started
+}
